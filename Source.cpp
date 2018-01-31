b1b4b214cc3aa0bf41e0ef48acfcdd7ff488c36a
Peter Pham <31864835+pizzapop2457@users.noreply.github.com>
2018-01-31 13:07:25 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -7,15 +7,11 @@ int main() {
 	ALLEGRO_DISPLAY *display = al_create_display(800, 800);
 	al_clear_to_color(al_map_rgb(255, 255, 255));
 
-	al_draw_filled_rectangle(100, 100, 700, 400, al_map_rgb(255, 165, 0));
-	al_draw_rectangle(100, 400, 700, 700, al_map_rgb(0, 0, 0), 5);
-	al_draw_filled_circle(250, 400, 100, al_map_rgb(255, 0, 0)); 
-	al_draw_filled_circle(550, 400, 100, al_map_rgb(255, 0, 0));
-	al_draw_filled_circle(250, 400, 50, al_map_rgb(0, 0, 0));
-	al_draw_filled_circle(550, 400, 50, al_map_rgb(0, 0, 0));
-	al_draw_filled_circle(400, 600, 50, al_map_rgb(0, 255, 0));
-	al_draw_filled_triangle(200, 250, 300, 250, 250, 150, al_map_rgb(0, 0, 255));
-	al_draw_filled_triangle(600, 250, 500, 250, 550, 150, al_map_rgb(0, 0, 255));
+	al_draw_filled_rectangle(100, 100, 700, 400, al_map_rgb(255, 0, 0));
+	al_draw_filled_circle(250, 400, 100, al_map_rgb(0, 255, 0)); 
+	al_draw_filled_circle(550, 400, 100, al_map_rgb(0, 255, 0));
+	al_draw_filled_triangle(150, 250, 250, 250, 200, 150, al_map_rgb(0, 0, 255));
+	al_draw_filled_triangle(650, 250, 550, 250, 600, 150, al_map_rgb(0, 0, 255));
 
 	al_flip_display();
 	al_rest(10);
