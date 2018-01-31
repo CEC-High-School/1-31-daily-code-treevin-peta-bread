f744a4064933f2d381946b8814f3f1bb225359ee
Peter Pham <31864835+pizzapop2457@users.noreply.github.com>
2018-01-31 13:02:42 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,23 @@
+#include <allegro5\allegro.h>
+#include <allegro5\allegro_primitives.h>
 
+int main() {
+	al_init();
+	al_init_primitives_addon(); //initialize helper library
+	ALLEGRO_DISPLAY *display = al_create_display(800, 800);
+	al_clear_to_color(al_map_rgb(255, 255, 255));
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+	al_draw_filled_rectangle(100, 100, 700, 400, al_map_rgb(255, 165, 0));
+	al_draw_rectangle(100, 400, 700, 700, al_map_rgb(0, 0, 0), 5);
+	al_draw_filled_circle(250, 400, 100, al_map_rgb(255, 0, 0)); 
+	al_draw_filled_circle(550, 400, 100, al_map_rgb(255, 0, 0));
+	al_draw_filled_circle(250, 400, 50, al_map_rgb(0, 0, 0));
+	al_draw_filled_circle(550, 400, 50, al_map_rgb(0, 0, 0));
+	al_draw_filled_circle(400, 600, 50, al_map_rgb(0, 255, 0));
+	al_draw_filled_triangle(200, 250, 300, 250, 250, 150, al_map_rgb(0, 0, 255));
+	al_draw_filled_triangle(600, 250, 500, 250, 550, 150, al_map_rgb(0, 0, 255));
+
+	al_flip_display();
+	al_rest(10);
+	al_destroy_display(display);
+}
